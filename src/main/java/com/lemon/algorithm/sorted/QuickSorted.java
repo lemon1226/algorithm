4f541e2316f2984b2501944dc803ddada201147d
zhangmeng <32800438@qq.com>
2020-06-10 23:48:12 +0800

    init

---
+++ src/main/java/com/lemon/algorithm/sorted/QuickSorted.java
@@ -0,0 +1,10 @@
+package com.lemon.algorithm.sorted;
+
+/**
+ * description: 快速排序
+ *
+ * @author isiah
+ * @date 2020-06-10 23:42:06 创建
+ */
+public class QuickSorted {
+}
---
+++ src/main/java/com/lemon/algorithm/sorted/SortedUtil.java
@@ -0,0 +1,22 @@
+package com.lemon.algorithm.sorted;
+
+import java.util.List;
+
+/**
+ * description: 公共类
+ *
+ * @author isiah
+ * @date 2020-06-10 23:43:06 创建
+ */
+public class SortedUtil {
+
+    public static boolean less(Comparable num1, Comparable num2) {
+        return num1.compareTo(num2) < 0;
+    }
+
+    public static void swap(List<Comparable> list, int index1, int index2) {
+        Comparable temp = list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, temp);
+    }
+}
